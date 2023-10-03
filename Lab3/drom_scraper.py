8fbf28435478631a0b4c870ef573996e854c7a02
Rashat Sarmanaev <110100353+Zysperro@users.noreply.github.com>
2023-10-03 17:48:55 +0500

    Add files via upload

---
+++ Lab3/drom_scraper.py
@@ -0,0 +1,27 @@
+import requests as rq
+import os
+from bs4 import BeautifulSoup as bs
+import base64
+import zipfile
+import uuid
+from datetime import datetime
+
+def GetWebPageByLink(url: str, folder_name = datetime.now().strftime("%Y-%m-%d - %H-%M")):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    response = rq.get(url=url)
+    if not response.ok:
+        return None
+    soup = bs(response.text, features="html.parser")
+    with zipfile.ZipFile('images.zip', 'w') as zpf:
+        for link in soup.find_all('img'):
+            img_link = link.get('src')
+            if(img_link[-3::] == "jpg"):
+                img = rq.get(img_link)
+                rng_name = str(uuid.uuid4())
+                if img.ok:
+                    with open(f"./{folder_name}/{rng_name}.png", "wb+") as fh:
+                        fh.write(base64.decodebytes(base64.b64encode(img.content)))
+                zpf.write(f"./{folder_name}/{rng_name}.png")
+
+GetWebPageByLink("https://auto.drom.ru/toyota/all/?maxprice=350000")
